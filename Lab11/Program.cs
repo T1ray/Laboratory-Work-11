7a5d3c4239c8cf5018873999f52b7176fc55c3ce
T1ray <tester2016go@gmail.com>
2025-03-23 00:11:18 +0500

    Лабораторная работа №11 #1

---
+++ Lab11/Program.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Lab10;
+using static AdditionalFunctions.AdditionalFunctions;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Task1()
+        {
+            Console.WriteLine("Задание 1: Неуниверсальная коллекция Queue");
+            
+            Queue queue = new Queue();
+            
+            queue.Enqueue(new MusicalInstrument("Скрипка", 1));
+            queue.Enqueue(new Guitar(6, 2));
+            queue.Enqueue(new ElectricGuitar("фиксированный источник питания", 6, 3));
+            queue.Enqueue(new Piano(88, "октавная", 4));
+            foreach (var instrument in queue)
+            {
+                Console.WriteLine(instrument);
+            }
+            Console.WriteLine("Элементы добавлены в очередь.");
+            Console.WriteLine("---");
+            
+            queue.Dequeue();
+            foreach (var instrument in queue)
+            {
+                Console.WriteLine(instrument);
+            }
+            Console.WriteLine("Первый элемент удален.");
+            Console.WriteLine("---");
+            
+            // Запрос 1: Среднее количество струн у гитар
+            int totalNumberStrings = 0;
+            int numberGuitars = 0;
+
+            foreach (var instrument in queue)
+            {
+                if (instrument is Guitar guitar)
+                {
+                    totalNumberStrings += guitar.NumberStrings;
+                    numberGuitars++;
+                }
+            }
+            Console.WriteLine("Среднее количество струн у гитар: ");
+            Console.Write(numberGuitars > 0 ? (double)totalNumberStrings / numberGuitars : 0 + "\n");
+            Console.WriteLine("---");
+
+            // Запрос 2: Общее количество струн на всех электрогитарах с фиксированным источником питания
+            totalNumberStrings = 0;
+
+            foreach (var instrument in queue)
+            {
+                ElectricGuitar? electricGuitar = instrument as ElectricGuitar;
+                if (electricGuitar != null && electricGuitar.PowerSource.ToLower() == "фиксированный источник питания")
+                {
+                    totalNumberStrings += electricGuitar.NumberStrings;
+                }
+            }
+            Console.WriteLine($"Общее количество струн на всех электрогитарах с фиксированным источником питания: {totalNumberStrings}");
+            Console.WriteLine("---");
+
+            // Запрос 3: Максимальное число с октавной раскладкой клавиш
+            int maxNumberKeys = 0;
+
+            foreach (var instrument in queue)
+            {
+                if (instrument.GetType() == typeof(Piano))
+                {
+                    Piano piano = (Piano)instrument;
+                    if (piano.KeyLayout == "октавная" && piano.NumberKeys > maxNumberKeys)
+                    {
+                        maxNumberKeys = piano.NumberKeys;
+                    }
+                }
+            }
+            Console.WriteLine($"Максимальное число с октавной раскладкой клавиш: {maxNumberKeys}");
+            Console.WriteLine("---");
+            
+            Console.WriteLine("Все элементы коллекции:");
+            foreach (var obj in queue)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+            Console.WriteLine("---");
+            
+            Queue clonedQueue = (Queue)queue.Clone();
+            Console.WriteLine("Коллекция(очередь) клонирована");
+            
+            ArrayList tempList = new ArrayList(queue);
+            tempList.Sort();
+            Console.WriteLine("Коллекция отсортирована (временный список):");
+            foreach (object obj in tempList)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+
+            MusicalInstrument searchInstrument = new Guitar(6, 2);
+            bool found = queue.Contains(searchInstrument);
+            Console.WriteLine(found
+                ? $"Элемент найден в очереди(Queue)"
+                : "Элемент не найден");
+        }
+        
+        static void Task2()
+        {
+            Console.WriteLine("Задание 2: Обобщенная коллекция SortedSet<MusicalInstrument>");
+            
+            SortedSet<MusicalInstrument> instrumentSet = new SortedSet<MusicalInstrument>();
+            
+            instrumentSet.Add(new Guitar(6, 5));
+            instrumentSet.Add(new ElectricGuitar("фиксированный источник питания", 6, 3));
+            instrumentSet.Add(new Piano(88, "октавная", 7));
+            instrumentSet.Add(new MusicalInstrument("Флейта", 8));
+            foreach (var instrument in instrumentSet)
+            {
+                Console.WriteLine(instrument);
+            }
+            Console.WriteLine("Элементы добавлены в SortedSet.");
+            Console.WriteLine("---");
+            
+            MusicalInstrument toRemove = new Guitar(6, 5);
+            instrumentSet.Remove(toRemove);
+            foreach (var instrument in instrumentSet)
+            {
+                Console.WriteLine(instrument);
+            }
+            Console.WriteLine("Элемент удален.");
+            Console.WriteLine("---");
+            
+            // Запрос 1: Среднее количество струн у гитар
+            int totalNumberStrings = 0;
+            int numberGuitars = 0;
+
+            foreach (var instrument in instrumentSet)
+            {
+                if (instrument is Guitar guitar)
+                {
+                    totalNumberStrings += guitar.NumberStrings;
+                    numberGuitars++;
+                }
+            }
+            Console.Write("Среднее количество струн у гитар: ");
+            Console.WriteLine(numberGuitars > 0 ? (double)totalNumberStrings / numberGuitars : 0);
+            
+            // Запрос 2: Общее количество струн на всех электрогитарах с фиксированным источником питания
+            totalNumberStrings = 0;
+
+            foreach (var instrument in instrumentSet)
+            {
+                ElectricGuitar? electricGuitar = instrument as ElectricGuitar;
+                if (electricGuitar != null && electricGuitar.PowerSource.ToLower() == "фиксированный источник питания")
+                {
+                    totalNumberStrings += electricGuitar.NumberStrings;
+                }
+            }
+            Console.WriteLine($"Общее количество струн на всех электрогитарах с фиксированным источником питания: {totalNumberStrings}");
+            
+            // Запрос 3: Максимальное число с октавной раскладкой клавиш
+            int maxNumberKeys = 0;
+
+            foreach (var instrument in instrumentSet)
+            {
+                if (instrument.GetType() == typeof(Piano))
+                {
+                    Piano piano = (Piano)instrument;
+                    if (piano.KeyLayout == "октавная" && piano.NumberKeys > maxNumberKeys)
+                    {
+                        maxNumberKeys = piano.NumberKeys;
+                    }
+                }
+            }
+            Console.WriteLine($"Максимальное число с октавной раскладкой клавиш: {maxNumberKeys}");
+            
+            Console.WriteLine("Все элементы коллекции (отсортированы по имени):");
+            foreach (MusicalInstrument mi in instrumentSet)
+            {
+                Console.WriteLine(mi);
+            }
+            
+            SortedSet<MusicalInstrument> clonedSet = new SortedSet<MusicalInstrument>(instrumentSet);
+            foreach (var instrument in clonedSet)
+            {
+                Console.WriteLine(instrument);
+            }
+            
+            MusicalInstrument searchInstrument = new ElectricGuitar("батарейки", 7, 6);
+            bool found = instrumentSet.Contains(searchInstrument);
+            Console.WriteLine(found
+                ? $"Элемент {searchInstrument} найден"
+                : "Элемент не найден 1");
+        }
+        
+        static void Task3()
+        {
+            TestCollections test = new TestCollections();
+
+            test.MeasureSearchTimes();
+        }
+
+        static void Menu()
+        {
+            Console.WriteLine("1. Выполнить 1-ую часть");
+            Console.WriteLine("2. Выполнить 2-ую часть");
+            Console.WriteLine("3. Выполнить 3-ую часть");
+        }
+        
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Menu();
+                TextSeparator();
+                Console.Write("Выберите пункт меню: ");
+                string input = Console.ReadLine();
+                TextSeparator();
+                switch (input)
+                {
+                    case "1":
+                        Task1();
+                        break;
+                    case "2":
+                        Task2();
+                        break;
+                    case "3":
+                        Task3();
+                        break;
+                }
+                TextSeparator();
+            }
+        }
+    }
+}
---
+++ Lab11/TestCollections.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab10;
+
+namespace Lab11
+{
+    public class TestCollections
+    {
+        private Queue<Guitar> col1;
+        private Queue<string> col2;
+        private SortedSet<MusicalInstrument> col3;
+        private SortedSet<string> col4;
+
+        private Guitar first;
+        private Guitar middle;
+        private Guitar last;
+        private Guitar notInCollection;
+
+        public TestCollections(int count = 1000)
+        {
+            col1 = new Queue<Guitar>();
+            col2 = new Queue<string>();
+            col3 = new SortedSet<MusicalInstrument>();
+            col4 = new SortedSet<string>();
+
+            Random rand = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                Guitar g = new Guitar(rand.Next(4, 17), i);
+                g.Name = $"Гитара{i}";
+
+                if (i == 0) first = (Guitar)g.Clone();
+                if (i == count / 2) middle = (Guitar)g.Clone();
+                if (i == count - 1) last = (Guitar)g.Clone();
+
+                col1.Enqueue(g);
+                col2.Enqueue(g.ToString());
+                col3.Add(g.GetBase);
+                col4.Add(g.ToString());
+            }
+
+            notInCollection = new Guitar(6, 1000) { Name = "Гитара1000" };
+        }
+
+        public void MeasureSearchTimes()
+        {
+            Console.WriteLine("Задание 3: Измерение времени поиска");
+
+            // Поиск в Queue<Guitar>
+            MeasureSearchQueueGuitar(first, "Первый элемент");
+            MeasureSearchQueueGuitar(middle, "Средний элемент");
+            MeasureSearchQueueGuitar(last, "Последний элемент");
+            MeasureSearchQueueGuitar(notInCollection, "Элемент вне коллекции");
+
+            // Поиск в Queue<string>
+            MeasureSearchQueueString(first, "Первый элемент");
+            MeasureSearchQueueString(middle, "Средний элемент");
+            MeasureSearchQueueString(last, "Последний элемент");
+            MeasureSearchQueueString(notInCollection, "Элемент вне коллекции");
+
+            // Поиск в SortedSet<MusicalInstrument>
+            MeasureSearchSortedSetGuitar(first, "Первый элемент");
+            MeasureSearchSortedSetGuitar(middle, "Средний элемент");
+            MeasureSearchSortedSetGuitar(last, "Последний элемент");
+            MeasureSearchSortedSetGuitar(notInCollection, "Элемент вне коллекции");
+
+            // Поиск в SortedSet<string>
+            MeasureSearchSortedSetString(first, "Первый элемент");
+            MeasureSearchSortedSetString(middle, "Средний элемент");
+            MeasureSearchSortedSetString(last, "Последний элемент");
+            MeasureSearchSortedSetString(notInCollection, "Элемент вне коллекции");
+        }
+
+        private void MeasureSearchQueueGuitar(Guitar searchedElement, string elementDescription)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                col1.Contains(searchedElement);
+            }
+            sw.Stop();
+            long averageTicks = sw.ElapsedTicks / 100;
+            Console.WriteLine($"Queue<Guitar> - {elementDescription}: {averageTicks} тиков");
+        }
+
+        private void MeasureSearchQueueString(Guitar searchedElement, string elementDescription)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                col2.Contains(searchedElement.ToString());
+            }
+            sw.Stop();
+            long averageTicks = sw.ElapsedTicks / 100;
+            Console.WriteLine($"Queue<string> - {elementDescription}: {averageTicks} тиков");
+        }
+
+        private void MeasureSearchSortedSetGuitar(Guitar searchedElement, string elementDescription)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                col3.Contains(searchedElement.GetBase);
+            }
+            sw.Stop();
+            long averageTicks = sw.ElapsedTicks / 100;
+            Console.WriteLine($"SortedSet<MusicalInstrument> - {elementDescription}: {averageTicks} тиков");
+        }
+
+        private void MeasureSearchSortedSetString(Guitar searchedElement, string elementDescription)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                col4.Contains(searchedElement.ToString());
+            }
+            sw.Stop();
+            long averageTicks = sw.ElapsedTicks / 100;
+            Console.WriteLine($"SortedSet<string> - {elementDescription}: {averageTicks} тиков");
+        }
+    }
+}
